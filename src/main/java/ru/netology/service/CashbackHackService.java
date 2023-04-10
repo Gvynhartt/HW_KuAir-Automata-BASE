f4d7edf16df065e2bc530fccb3d456426cff7e2d
Gvynhartt <savshush134@gmail.com>
2023-04-10 13:24:50 +0300

    kavo?

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -4,6 +4,6 @@ public class CashbackHackService {
     private final int sumLimit = 1000;
 
     public int sumLeft(int totalSum) {
-        return sumLimit - totalSum % sumLimit;
+        return sumLimit - totalSum % sumLimit; // зачем нам остаток от деления и почему мы его вычитаем?
     }
 }
