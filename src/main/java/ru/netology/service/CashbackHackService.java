e35f73425c64b80db31215f76ca11c5cb1a9966b
Gvynhartt <savshush134@gmail.com>
2023-04-10 21:10:23 +0300

    test written with TestNG

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -4,6 +4,7 @@ public class CashbackHackService {
     private final int sumLimit = 1000;
 
     public int sumLeft(int totalSum) {
-        return sumLimit - totalSum % sumLimit; // зачем нам остаток от деления и почему мы его вычитаем?
+        return sumLimit - totalSum % sumLimit; /** Где хоть одно условие, проверяемое перед вычислениями?
+         С другой стороны, нет бранчей - значет, и насчёт покрытия чесаться не надо */
     }
 }
